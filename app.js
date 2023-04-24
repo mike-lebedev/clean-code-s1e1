3ef585b450e72b52783c1aaefc1a2a2f249a6855
mike-lebedev <3lectrod@mail.ru>
2023-04-24 16:20:06 +0300

    fix(extended-1.2): add atributs fo img

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ var createNewTaskElement = function (taskString) {
   //button.delete
   var deleteButton = document.createElement("button"); //delete button
   var deleteButtonImg = document.createElement("img"); //delete button image
+  deleteButtonImg.setAttribute("alt","Remove button")
 
   label.innerText = taskString;
   label.className = "task";
